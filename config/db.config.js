d5f842e495f1e279d52588cab403533530978f2d
Diogo Teixeira <teixeira.diogo5@gmail.com>
2020-05-25 12:39:50 +0100

    init

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27018/node-express-api",
+  url:
+    "mongodb://u8iggk4ad3fvvrxrrfv3:gjoZJQ3WKLJaiX24ub4L@bhzj9uqzbf4qxna-mongodb.services.clever-cloud.com:27017/bhzj9uqzbf4qxna",
 };
